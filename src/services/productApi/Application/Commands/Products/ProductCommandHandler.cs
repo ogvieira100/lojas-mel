0a23369480490a23a3766cd1018e27ea227b96b1
ovieira <ovieira.nava@bancomaster.com.br>
2024-02-07 21:05:44 -0300

    criação do produtos

--- src/services/productApi/Application/Commands/Products/ProductCommandHandler.cs
+++ src/services/productApi/Application/Commands/Products/ProductCommandHandler.cs
@@ -1,6 +1,45 @@
-﻿namespace productApi.Application.Commands.Products
+﻿using AutoMapper;
+using buildingBlocksCore.Data.PersistData.Interfaces;
+using buildingBlocksCore.Mediator;
+using buildingBlocksCore.Mediator.Messages;
+using buildingBlocksCore.Models;
+using buildingBlocksCore.Utils;
+using FluentValidation;
+using MediatR;
+
+namespace productApi.Application.Commands.Products
 {
-    public class ProductCommandHandler
+    public class ProductCommandHandler :
+        IRequestHandler<InsertProductCommand, ResponseCommad<InsertProductResponseCommand>>,
+        IRequestHandler<UpdateProductCommand, ResponseCommad<object>>,
+        IRequestHandler<DeleteProductCommand, ResponseCommad<object>>
     {
+
+        readonly IBaseRepository<Produto> _produtoRepository;
+        readonly IMapper _mapper;
+        readonly buildingBlocksCore.Identity.IUser _user;
+        readonly LNotifications _notifications;
+        readonly IMediatorHandler _mediatorHandler;
+        readonly IValidator<InsertProductCommand> _validatorInsertProductCommand;
+        readonly IValidator<UpdateProductCommand> _validatorUpdateProductCommand;
+        readonly ILogger<ProductCommandHandler> _logger;
+        public ProductCommandHandler(ILogger<ProductCommandHandler> logger)
+        {
+            _logger = logger;   
+        }
+        public async Task<ResponseCommad<InsertProductResponseCommand>> Handle(InsertProductCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ResponseCommad<object>> Handle(UpdateProductCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async  Task<ResponseCommad<object>> Handle(DeleteProductCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
